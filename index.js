a97e2d5b5a2e816e002b917cf7419b92b92b118b
Rachel Alpert <ambitious.fishus@gmail.com>
2017-05-13 10:36:18 -0400

    Basic sentence generation working

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const bot = new Discord.Client();
 const markov = new rita.RiMarkov(3);
 var data = '';
 
-const markovListener = fs.createWriteStream('markov.txt', {flags: 'a'});
-var readStream = fs.createReadStream('markov.txt', 'utf8');
+const markovListener = fs.createWriteStream('./data/markov.txt', {flags: 'a'});
+var readStream = fs.createReadStream('./data/markov.txt', 'utf8');
 
 readStream.on('data', function(chunk) {  
     data += chunk;
@@ -25,26 +25,7 @@ bot.on('message', (msg) => {
   if (msg.mentions.users.find(val => val.id === secret.botId) && !msg.author.bot) {
     let rs = new rita.RiString(msg.content);
     let words = rs.words();
-    console.log(words);
-    // let words = rs.words();
-    // console.log(words);
-    // let pos = words.pos();
-    // let tokens = '';
-    // for (let i = 0; i < words.length; i++) {
-    //   if (pos[i] === 'nn')
-    //     // msg.channel.sendMessage('aloha');
-    //     tokens += i;
-    // }
-    // console.log(tokens);
-    if (words.length >0) {
-    //   for (let i = 0; i < (tokens.length); i++) {
-        msg.channel.sendMessage(markov.loadTokens(words).generateSentences(1));
-      //   console.log(tokens[i]);
-      // }
-      // msg.channel.sendMessage('Where is your god now?');
-    }
-    else
-      msg.channel.sendMessage(markov.generateSentences(1));
+    msg.channel.sendMessage(markov.generateSentences(1));
   }
 });
 
